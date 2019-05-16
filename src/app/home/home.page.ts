b517ca867abc493e7aff34cebcb77b980e6cffd7
abdoulk9 <abdoulkadre@yahoo.fr>
2019-05-16 02:14:41 +0200

    affichage villes

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+import { Z_FIXED } from 'zlib';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -7,4 +8,55 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  public townTab ;
+  public townSelect;
+
+
+  constructor(private httpClient: HttpClient) {
+
+  }
+
+  ionViewDidEnter() {
+    this.httpClient.get("http://localhost:4000/villes")
+      .subscribe(
+        (data: any) => {
+          console.log(data);
+          this.townTab = data;
+          
+        },
+        err => console.log(err)
+      );
+     console.log(this.townTab);
+  }
+
+  validateSelect(id) {
+    console.log(" voici l'id:" + id);
+    if (id > 0) {
+      this.httpClient.get("http://localhost:4000/ville/" + id)
+        .subscribe(
+          (data: any) => {
+            console.log(data)
+            if (data) {
+              this.townSelect = data;
+              console.log(this.townSelect);
+            }
+            err => console.log(err)
+
+            console.log(this.townSelect);
+          })
+    }
+
+  }
+
+
+displayTown(town: any){
+  console.log(town);
 }
+
+
+}
+
+
+
+
+
